8053fd690e093949dcf417bf59a70609dc13c594
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-09-11 18:30:49 -0500

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,9 @@ export const initialState = {
   additionalPrice: 0,
   car: {
     price: 50000.00,
-    name: '1990 Fiat Multipla',
+    name: '2019, BMW M3 Or something IDK',
     image:
-      'https://secure.i.telegraph.co.uk/multimedia/archive/00842/motoring-graphics-2_842469a.jpg',
+      'https://static.carsdn.co/cldstatic/wp-content/uploads/img1937906909-1540911513852.jpg',
     features: []
   },
   store: [
@@ -14,7 +14,7 @@ export const initialState = {
     { id: 4, name: 'Rear spoiler', price: 250 },
     {id: 5, name: 'some real big spinners', price: 260},
     {id: 6, name: 'those bright af for no reason headlights that annoy everyone infront of you guarenteed', price:500},
-    {id: 7, name:'A lifetime of fulfillment', price: 'Priceless'},
+    {id: 7, name:'A lifetime of fulfillment', price: '100000'},
     {id: 8, name: 'Turn signals that show the opposite of whichever way you are turning.', price: 25.00}
   ]
 }
